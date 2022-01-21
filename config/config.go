3b5a140374e00ad08a482a5faffd948747ad4b1d
Katsumi-N <naya.katsumi@gmail.com>
2022-01-21 11:20:54 +0900

    edit go.mod for deploying heroku

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"log"
 	"os"
-
-	"gopkg.in/ini.v1"
 )
 
 type ConfigList struct {
@@ -21,20 +18,33 @@ type ConfigList struct {
 var Config ConfigList
 
 func init() {
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		log.Printf("Failed to read file: %v", err)
-		os.Exit(1)
-	}
+	// cfg, err := ini.Load("config.ini")
+	// if err != nil {
+	// 	log.Printf("Failed to read file: %v", err)
+	// 	os.Exit(1)
+	// }
+
+	// Config = ConfigList{
+	// 	TenantId:       cfg.Section("conoha").Key("tenantId").String(),
+	// 	ServerEndpoint: cfg.Section("conoha").Key("server_endpoint").String(),
+	// 	ServerId:       cfg.Section("conoha").Key("serverId").String(),
+	// 	Username:       cfg.Section("conoha").Key("username").String(),
+	// 	Password:       cfg.Section("conoha").Key("password").String(),
+	// 	Flavor1gb:      cfg.Section("conoha").Key("flavor_1gb").String(),
+	// 	Flavor4gb:      cfg.Section("conoha").Key("flavor_4gb").String(),
+	// 	DiscordToken:   cfg.Section("discord").Key("token").String(),
+	// }
+
+	// deploying for heroku
 
 	Config = ConfigList{
-		TenantId:       cfg.Section("conoha").Key("tenantId").String(),
-		ServerEndpoint: cfg.Section("conoha").Key("server_endpoint").String(),
-		ServerId:       cfg.Section("conoha").Key("serverId").String(),
-		Username:       cfg.Section("conoha").Key("username").String(),
-		Password:       cfg.Section("conoha").Key("password").String(),
-		Flavor1gb:      cfg.Section("conoha").Key("flavor_1gb").String(),
-		Flavor4gb:      cfg.Section("conoha").Key("flavor_4gb").String(),
-		DiscordToken:   cfg.Section("discord").Key("token").String(),
+		TenantId:       os.Getenv("conohatenantId"),
+		ServerEndpoint: os.Getenv(""),
+		ServerId:       os.Getenv("conohaserverId"),
+		Username:       os.Getenv("conohausername"),
+		Password:       os.Getenv("conohapassword"),
+		Flavor1gb:      os.Getenv("conohaflavor4gb"),
+		Flavor4gb:      os.Getenv("conohaflavor1gb"),
+		DiscordToken:   os.Getenv("discordtoken"),
 	}
 }
